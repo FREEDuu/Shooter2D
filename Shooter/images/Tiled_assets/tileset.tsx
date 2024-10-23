da58f4f2733827c656747c12c7e4de4bbe5985e5
T0k4m4K <luca.bedini02@gmail.com>
2024-10-23 17:00:47 +0200

    mappe aggiornate

--- Shooter/images/Tiled_assets/tileset.tsx
+++ Shooter/images/Tiled_assets/tileset.tsx
@@ -2,24 +2,24 @@
 <tileset version="1.10" tiledversion="1.11.0" name="tileset" tilewidth="16" tileheight="16" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image source="../Environment/Tiles/000.png" width="16" height="16"/>
+  <image source="../Environment/Tiles/0.png" width="16" height="16"/>
  </tile>
  <tile id="1">
-  <image source="../Environment/Tiles/001.png" width="16" height="16"/>
+  <image source="../Environment/Tiles/1.png" width="16" height="16"/>
  </tile>
  <tile id="2">
-  <image source="../Environment/Tiles/002.png" width="16" height="16"/>
+  <image source="../Environment/Tiles/2.png" width="16" height="16"/>
  </tile>
  <tile id="3">
-  <image source="../Environment/Tiles/003.png" width="16" height="16"/>
+  <image source="../Environment/Tiles/3.png" width="16" height="16"/>
  </tile>
  <tile id="4">
-  <image source="../Environment/Tiles/004.png" width="16" height="16"/>
+  <image source="../Environment/Tiles/4.png" width="16" height="16"/>
  </tile>
  <tile id="5">
-  <image source="../Environment/Tiles/005.png" width="16" height="16"/>
+  <image source="../Environment/Tiles/5.png" width="16" height="16"/>
  </tile>
  <tile id="6">
-  <image source="../Environment/Tiles/006.png" width="16" height="16"/>
+  <image source="../Environment/Tiles/6.png" width="16" height="16"/>
  </tile>
 </tileset>
